88c53da722fb0ef9168fff349eb89a0b81904acc
Bjørn <bso@lunar.app>
2020-07-25 20:11:57 +0200

    Idempotency

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
-echo -n "Checking bitbucket access"
+echo -n "Checking BitBucket access"
 bitbucket_access=$(ssh -T git@bitbucket.org 2>&1)
 if [[ $bitbucket_access =~ "^logged in as" ]]; then
-    echo " - seems good"
+    echo " - OK"
 else
     echo ""
     echo "-------------------------------------------------------"
@@ -11,56 +11,70 @@ else
     echo "Consider 'ssh-add -K' or updating your .ssh/config like described at https://nerderati.com/2011/03/17/simplify-your-life-with-an-ssh-config-file/"
     echo "-------------------------------------------------------"
 fi
-echo -n "Checking github access"
+echo -n "Checking GitHub access"
 github_access=$(ssh -T git@github.com 2>&1)
 if [[ $github_access =~ "^Hi " ]]; then
-    echo " - seems good"
+    echo " - OK"
 else
     echo ""
     echo "-------------------------------------------------------"
-    echo "Please fix access to github an try again."
+    echo "Please fix access to GitHub an try again."
     echo "Consider 'ssh-add -K' or updating your .ssh/config like described at https://nerderati.com/2011/03/17/simplify-your-life-with-an-ssh-config-file/"
     echo "-------------------------------------------------------"
     return
 fi
 
 # we expect some version of zsh is installed
-echo "Installing zplug"
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+if [ -z "$ZPLUG_HOME" ]; then
+    echo "Installing zplug"
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+else
+    echo "Updating zplug"
+    (cd $ZPLUG_HOME && git pull --ff-only)
+fi
+
 echo ""
 echo "Loading zplug"
-source ~/.zplug/init.zsh
+source $HOME/.zplug/init.zsh
 echo ""
 echo "Installing lw-zsh"
 ZPLUG_PROTOCOL=ssh
 export LC_ALL= && export LANG=en_US.UTF-8
 zplug "lunarway/lw-zsh"
-if [ ! -d ~/.zplug/repos/lunarway/lw-zsh ] && [ ! -d ~/.zplug/repos/lunarway/lw-zsh ]; then
+if [ ! -d $HOME/.zplug/repos/lunarway/lw-zsh ] && [ ! -d $HOME/.zplug/repos/lunarway/lw-zsh ]; then
     zplug install
 else
     zplug update lunarway/lw-zsh
 fi
 echo ""
 echo "Installing default plugins"
-source ~/.zplug/repos/lunarway/lw-zsh/default-plugins.zsh
+source $HOME/.zplug/repos/lunarway/lw-zsh/default-plugins.zsh
 zplug install
-echo ""
-echo "Moving existing ~/.zshrc"
-backup=~/.zshrc.$(date +%Y-%m-%d_%H-%M-%S).backup
-cp ~/.zshrc $backup
+
+# Backup existing .zshrc if it exists
+if [ -f $HOME/.zshrc ]; then
+    echo ""
+    backup=$HOME/.zshrc.$(date +%Y-%m-%d_%H-%M-%S).backup
+    echo "Moving existing $HOME/.zshrc into $backup"
+    cp $HOME/.zshrc $backup
+    backup_message='
+To re-apply your customized aliases and more copy stuff back from '$backup' to '$HOME'/.zshrc.
+'
+fi
+
 echo ""
 echo "Generating new .zshrc"
 vared -p "Please specify your Lunar email: " -c tmp
-cp ~/.zplug/repos/lunarway/lw-zsh/.zshrc.example ~/.zshrc
-sed -i -- "s/your-initials@lunarway.com/$tmp/g" ~/.zshrc
+cp $HOME/.zplug/repos/lunarway/lw-zsh/.zshrc.example $HOME/.zshrc
+
+sed -i '' "s/your-initials@lunarway.com/$tmp/g" $HOME/.zshrc
+
 echo ""
 echo "---------------------------------------------------------------------------------------------------------"
-echo "Lunar's default zsh is installed"
+echo "Lunar's default zsh configuration is installed"
+echo "$backup_message"
+echo "Follow font and iterm profile setup at https://github.com/lunarway/lw-zsh"
 echo ""
-echo "To re-apply your customized alias' and more copy stuff back from $backup to ~/.zshrc."
-echo ""
-echo "Follow font and iTerm profile setup at https://github.com/lunarway/lw-zsh"
-echo ""
-echo "Close this terminal and open a new one"
+echo "Close this terminal and re-open a new one"
 echo "---------------------------------------------------------------------------------------------------------"
 echo
